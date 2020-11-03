e0cc3439708e325ea6c5a783b7e94c3ed337d111
qt_95 <irkinils95@gmail.com>
2020-11-03 20:15:26 +0300

    MASTER

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+// let userName = prompt("what is ur name?");
+
+// alert( `ur name is ${userName, 2**6}` ); // true, если нажата OK
+// sdfgdg
+// dfgdg
+// !let currentUserName = "Джон";
+// ?currentUserName = 'asdasd';
+// todo: alert(currentUserName);
+
+
+let elem = createElement();
+setHandler(elem);
+walkAround();
+
+// --- вспомогательные функции ---
+function cReateElement() {
+  // sdfsdf
+
+}
+
+function setHandler(elem) {
+
+}
+
+
+function walkAround() {
+
+}
+
